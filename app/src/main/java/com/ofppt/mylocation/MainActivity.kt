751e6ea0d2b8f90a47fcbcc9a946ba174e10a5bc
Nizar <ettaheri.nizar@gmail.com>
2022-11-18 23:00:30 +0100

    chore: clean permissions declarations'

--- app/src/main/java/com/ofppt/mylocation/MainActivity.kt
+++ app/src/main/java/com/ofppt/mylocation/MainActivity.kt
@@ -24,50 +24,10 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<View>(R.id.open_map).setOnClickListener( OnClickListener
         {
-            Dexter.withContext(this).withPermissions(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ).withListener(object : MultiplePermissionsListener
-            {
-                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
-                    Toast.makeText(this@MainActivity," Permission checked",Toast.LENGTH_LONG).show()
+            supportFragmentManager.beginTransaction().replace(R.id.frame, MapFragment(),"MapFragment").addToBackStack("MapFragment").commit()
 
-                    Dexter.withContext(this@MainActivity).withPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-                        .withListener(object : PermissionListener{
-                            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                                supportFragmentManager.beginTransaction().replace(R.id.frame, MapFragment(),"MapFragment").addToBackStack("MapFragment").commit()
-
-                            }
-
-                            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
-                               p0?.isPermanentlyDenied
-                            }
-
-                            override fun onPermissionRationaleShouldBeShown(
-                                p0: PermissionRequest?,
-                                p1: PermissionToken?
-                            ) {
-                                p1?.continuePermissionRequest()
-                            }
-
-                        }).check()
-
-
-
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    p0: MutableList<PermissionRequest>?,
-                    p1: PermissionToken?
-                ) {
-                    p1?.continuePermissionRequest()
-                }
-
-            }
-
-            ).check()
-
-        })
+        }
+        )
 
 
 
